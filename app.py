b62a99e2e095cab551d557048c8b9c4dab88e35f
Vinod Karantothu <vinod.karantothu@live.in>
2021-04-05 09:06:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,64 +11,64 @@ from flask import Flask, Response, render_template, g
 app = Flask(__name__)
 
 conn = None
-final_frame = b''
+cam_frame = b''
+stop_threads = False
+
+VIDEO_STREAM_HOST = '0.0.0.0'
+VIDEO_STREAM_SOCKET_PORT = '8096'
 
 def create_listener():
-    global conn
-    global final_frame
+    global conn, cam_frame, stop_threads
     
-    HOST='localhost'
-    PORT=8096
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((VIDEO_STREAM_HOST, VIDEO_STREAM_SOCKET_PORT))
+        print('Socket bind complete')
+        s.listen(10)
+        print('Socket now listening')
 
-    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    print('Socket created')
+        while True:
+            conn, addr = s.accept()
+            data = b""
+            payload_size = struct.calcsize(">L")
 
-    s.bind((HOST,PORT))
-    print('Socket bind complete')
-    s.listen(10)
-    print('Socket now listening')
+            if stop_threads:
+                conn.close()
+                break
 
-    while True:        
-        conn,addr=s.accept()
-
-        data = b""
-        payload_size = struct.calcsize(">L")
-        print("payload_size: {}".format(payload_size))
-        # return conn, data, payload_size
-
-        try:
             while True:
+                counter = 0
                 while len(data) < payload_size:
-                    print("Recv: {}".format(len(data)))
+                    counter += 1
                     data += conn.recv(4096)
+                    if counter == 10:
+                        break
+                if counter == 10:
+                    break
 
                 print("Done Recv: {}".format(len(data)))
                 packed_msg_size = data[:payload_size]
                 data = data[payload_size:]
                 msg_size = struct.unpack(">L", packed_msg_size)[0]
-                print("msg_size: {}".format(msg_size))
+                # print("msg_size: {}".format(msg_size))
                 while len(data) < msg_size:
                     data += conn.recv(4096)
                 frame_data = data[:msg_size]
                 data = data[msg_size:]
-
-                frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
-                print(type(frame))
+                frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
                 im = Image.fromarray(frame)
-                # frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-                
-                # ret, jpeg = cv2.imencode('.jpg', frame)
-                final_frame = im.tobytes()  # jpeg.tobytes()
-                # cv2.imshow('ImageWindow',frame)
-                # cv2.waitKey(1)
-        finally:
-            conn.close()
+                cam_frame = im.tobytes()  # jpeg.tobytes()
+                print('Cam1 Feed RECEIVED')
+
+                if stop_threads:
+                    conn.close()
+                    break
 
 def get_frame():
-    global final_frame
+    global cam_frame
     while True:
-        print(final_frame)    
-        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + final_frame + b'\r\n\r\n')
+        # print(cam_frame)    
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + cam_frame + b'\r\n\r\n')
 
 
 @app.route('/')
